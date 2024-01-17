970c714115941498d8099dfac7216b20c6ab2b72
josemedina1 <medinavera.jose@gmail.com>
2024-01-17 18:11:28 -0300

    tabla index

--- formulario/crud/views.py
+++ formulario/crud/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render, redirect
-from .forms import PersonaForm
 from .models import Persona
+from .forms import PersonaForm
 
 def index(request):
+    error_message = None
+
     if request.method == 'POST':
-        formulario = PersonaForm(request.POST)
-        if formulario.is_valid():
-            formulario.save()
-            return redirect('index')  
-        # Redirigir a index.html
+        form = PersonaForm(request.POST)
+        if form.is_valid():
+            # Validar si está vacio el formulario
+            '''
+                form.cleaned_data['nombre_input']
+                se obtiene el valor del campo por ejemplo 'name' del formulario
+                después devalidar "form.is_valid()". 
+            '''
+            if not form.cleaned_data['name'] and not form.cleaned_data['lastname'] and not form.cleaned_data['city'] and not form.cleaned_data['country']:
+                # El formulario está vacío, realiza alguna acción (por ejemplo, muestra un mensaje de error)
+                error_message = "El formulario está vacío. Todos los campos son obligatorios."
+            else:
+                # Guardar los datos en la base de datos u otras acciones necesarias
+                form.save()
+                return redirect('index')
     else:
         form = PersonaForm()
 
     person = Persona.objects.all()
-    
-    return render(request, 'index.html', {'form': form, 'person':person})
+
+    return render(request, 'index.html', {'form': form, 'person': person, 'error_message': error_message})
